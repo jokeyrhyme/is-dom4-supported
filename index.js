b4f3d1b932a34e6baa1f768b8e1e19de32922e46
Ron Waldon <jokeyrhyme@gmail.com>
2015-06-04 09:21:07 +1000

    improve compatibility with IE (10, 11), Edge

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
 /**
  * http://www.w3.org/TR/dom/#interface-customevent
- * @type {Boolean} are CustomEvents supported?
+ * @param {Element} [el] a DOM Element to run tests against
+ * @returns {Boolean} are CustomEvents supported?
  */
-var customEvents = (function () {
-  var NAME = 'foo';
+var customEvents = function (el) {
+  var doc = el.ownerDocument || global.document;
+  var NAME = 'customevent';
   var event;
   try {
-    event = new global.CustomEvent(NAME);
+    event = doc.createEvent(NAME);
     return !!(
-      event.type === NAME &&
+      typeof event.type === 'string' &&
       typeof event.stopPropagation === 'function' &&
       typeof event.stopImmediatePropagation === 'function' &&
       typeof event.preventDefault === 'function'
@@ -18,7 +20,7 @@ var customEvents = (function () {
   } catch (ignore) {
     return false;
   }
-}());
+};
 
 /**
  * http://www.w3.org/TR/dom/#interface-eventtarget
@@ -58,17 +60,16 @@ var documents = function (el) {
  * http://www.w3.org/TR/dom/#interface-parentnode
  * http://www.w3.org/TR/dom/#interface-childnode
  * @param {Element} [el] a DOM Element to run tests against
- * @returns {Boolean} are ParentNodes, ChildNodes and Nodes supported?
+ * @returns {Boolean} are ParentNodes and Nodes supported?
  */
 var nodes = function (el) {
+  // we don't test ChildNode#remove(), because that's a ShadowDOM thing
   return !!(
     // ParentNode
     typeof el.children === 'object' &&
     typeof el.childElementCount === 'number' &&
     typeof el.querySelector === 'function' &&
     typeof el.querySelectorAll === 'function' &&
-    // ChildNode
-    typeof el.remove === 'function' &&
     // Node
     global.Node &&
     typeof global.Node.ELEMENT_NODE === 'number' &&
@@ -95,7 +96,13 @@ var elements = function (el) {
     typeof el.tagName === 'string' &&
     typeof el.className === 'string' &&
     typeof el.classList === 'object' &&
-    typeof el.matches === 'function' &&
+    (
+      typeof el.matches === 'function' ||
+      typeof el.matchesSelector === 'function' ||
+      typeof el.webkitMatchesSelector === 'function' ||
+      typeof el.mozMatchesSelector === 'function' ||
+      typeof el.msMatchesSelector === 'function' // IE (10, 11), Edge
+    ) &&
     typeof el.getAttribute === 'function' &&
     typeof el.hasAttribute === 'function' &&
     typeof el.removeAttribute === 'function' &&
@@ -116,7 +123,7 @@ module.exports = function (el) {
     }
   }
   return !!(
-    customEvents &&
+    customEvents(el) &&
     eventTargets(el) &&
     documents(el) &&
     nodes(el) &&
